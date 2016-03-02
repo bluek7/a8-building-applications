ff1ecc47393da267b801bfc38a4eae0ce9ad1d22
Kush Gupta <bluek7@D-10-157-160-26.dhcp4.washington.edu>
2016-03-02 03:35:39 -0800

    Done, just comments left

--- server.R
+++ server.R
@@ -1,2 +1,19 @@
 library(shiny)
 library(plotly)
+library(dplyr)
+
+
+shinyServer(function(input, output) {
+  
+  output$plotScatter <- renderPlotly({
+    data <- iris %>% filter(Species == input$species)
+    data %>% plot_ly(type = 'scatter',
+                     x = eval(parse(text = input$x)), 
+                     y = eval(parse(text = input$y)),
+                     mode = 'markers') %>% 
+      layout(yaxis = list(title = input$y),
+             xaxis = list(title = input$x),
+             title = paste0(input$species, ": ", input$y, " v.s. ", input$x))
+  })  
+
+})
--- ui.R
+++ ui.R
@@ -1,2 +1,35 @@
 library(shiny)
 library(plotly)
+
+choicelist <- list("Sepal Length" = "Sepal.Length", 
+                   "Sepal Width" = "Sepal.Width",
+                   "Petal Length" = "Petal.Length",
+                   "Petal Width" = "Petal.Width")
+
+shinyUI(fluidPage(
+  
+  titlePanel("Iris"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      radioButtons("y", label = h3("y-Axis"),
+                   choices = choicelist, 
+                   selected = "Sepal Length"),
+      
+      radioButtons("x", label = h3("x-Axis"),
+                   choices = choicelist, 
+                   selected = "Sepal Length"),
+      
+      radioButtons("species", label = h3("Species"),
+                   choices = list("Setosa" = "setosa",
+                                  "Versicolor" = "versicolor",
+                                  "Virginica" = "virginica"), 
+                   selected = "Setosa")
+    ),
+    
+    mainPanel(
+      plotlyOutput("plotScatter")
+    )
+    )
+))
