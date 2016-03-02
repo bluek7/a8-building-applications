79458ed6c2ebaaead648af2e454081ecd71dd82d
Kush Gupta <bluek7@D-10-157-160-26.dhcp4.washington.edu>
2016-03-02 03:40:58 -0800

    Added comments

--- server.R
+++ server.R
@@ -1,16 +1,23 @@
+# Including libraries
 library(shiny)
 library(plotly)
 library(dplyr)
 
-
+# Defining the server of the app
 shinyServer(function(input, output) {
   
+  # Defining the scatterplot graph function
   output$plotScatter <- renderPlotly({
+    # Filtering relevant data
     data <- iris %>% filter(Species == input$species)
+    
+    # Making scatterplot using plotly
     data %>% plot_ly(type = 'scatter',
                      x = eval(parse(text = input$x)), 
                      y = eval(parse(text = input$y)),
                      mode = 'markers') %>% 
+      
+      # Defining the layout of the graph
       layout(yaxis = list(title = input$y),
              xaxis = list(title = input$x),
              title = paste0(input$species, ": ", input$y, " v.s. ", input$x))
--- ui.R
+++ ui.R
@@ -1,35 +1,47 @@
+# Including libraries
 library(shiny)
 library(plotly)
 
+# Making list for choices for x and y axis variables
 choicelist <- list("Sepal Length" = "Sepal.Length", 
                    "Sepal Width" = "Sepal.Width",
                    "Petal Length" = "Petal.Length",
                    "Petal Width" = "Petal.Width")
 
+# Defining fluid page
 shinyUI(fluidPage(
   
-  titlePanel("Iris"),
+  # Setting the title of the app
+  titlePanel("Iris Analysis"),
   
+  # Defining the sidebar layout
   sidebarLayout(
     
+    # Defining sidebar panel
     sidebarPanel(
+      
+      # Radio button widget for y-axis
       radioButtons("y", label = h3("y-Axis"),
                    choices = choicelist, 
                    selected = "Sepal Length"),
       
+      # Radio button widget for x-axis
       radioButtons("x", label = h3("x-Axis"),
                    choices = choicelist, 
                    selected = "Sepal Length"),
       
+      # Radio button widget for species
       radioButtons("species", label = h3("Species"),
                    choices = list("Setosa" = "setosa",
                                   "Versicolor" = "versicolor",
                                   "Virginica" = "virginica"), 
                    selected = "Setosa")
-    ),
+      ),
     
+    # Defining main panel
     mainPanel(
-      plotlyOutput("plotScatter")
-    )
+      # Making scatter plot
+        plotlyOutput("plotScatter")
+      )
     )
 ))
